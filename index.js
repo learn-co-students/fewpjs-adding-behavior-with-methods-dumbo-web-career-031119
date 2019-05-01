5c1f0551ed7fb813a97660e0b99a65e439de27b6
github username <itsDario1@gmail.com>
2019-05-01 13:07:32 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Your code here
+
+class Cat {
+    constructor(name, sex) {
+        self.name = name
+        self.sex = sex
+    }
+
+    speak() {
+        return `${name} says meow!`
+    }
+}
+
+class Dog {
+    constructor(name, sex) {
+        self.name = name
+        self.sex = sex
+    }
+
+    speak() {
+        return `${name} says woof!`
+    }
+}
+
+class Bird {
+    constructor(name, sex) {
+        this.name = name
+        this.sex = sex
+    }
+
+    speak() {
+        let response = ``;
+        console.log(this.sex, this.name)
+        // console.log(self)
+        if (this.sex == 'male') {
+            response = `It's me! ${this.name}, the parrot!`
+        } else if (this.sex === 'female') {
+            response = `${this.name} says squawk!`
+        }
+        return response
+    }
+}
